9c01b573fbbeb94b92b15ae337a56a3ad3f1087f
abhikumar18 <abhishek.it.msit@gmail.com>
2024-08-24 11:52:33 +0100

    Structure ready

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-rn';
+
+export default function App() {
+  return (
+    <View style={tw('flex-1 justify-center items-center')}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
